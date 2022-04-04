2ae6145b60cceb530a3b384ca3c92aae115f044d
Priyanjli <priyanjalimourya@gmail.com>
2022-04-04 23:28:03 +0800

    lint fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
-    return (
-        <div>
-                 <h1>Welcome contact Page</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Welcome contact Page</h1>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
